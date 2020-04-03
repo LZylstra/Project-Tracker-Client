811391171aeee7896e2ee776ac7c295a9e7140ce
joshua hahn <tailrin.jh@gmail.com>
2020-04-02 23:55:17 -0600

    more work on app.js

--- src/App/App.js
+++ src/App/App.js
@@ -12,13 +12,89 @@ import ProjectPage from '../ProjectPage/ProjectPage';
 import TaskPage from '../TaskPage/TaskPage';
 import AddProject from '../AddProject/AddProject';
 import AddTask from '../AddTask/AddTask';
+import config from '../config';
 
 class App extends Component { 
-  state ={
-    tasks: [],
-    projects: [],
-    userId: null,
-    userIsAdmin: false
+
+  //add persistance to state by storing state in localStorage
+  constructor(props){
+    super(props);
+    const initialState = {
+      tasks: [],
+      projects: [],
+      loggedIn: !!window.sessionStorage.jwt,
+      userId: null,
+      userIsAdmin: false,
+      companyId: null,
+      isModbile: true
+    }
+    //if state isent present in localStorage use that otherwise use initial state
+    this.state = JSON.parse(sessionStorage.getItem('state'))
+      ? JSON.parse(sessionStorage.getItem('state'))
+      : initialState
+
+    //override this.setState method to save state to local storage with every update
+    const orginial = this.setState;     
+    this.setState = function() {
+      let arguments0 = arguments[0];
+      let arguments1 = () => (arguments[1], sessionStorage.setItem('state', JSON.stringify(this.state)));
+      orginial.bind(this)(arguments0, arguments1);
+    };
+  }
+  
+
+  //Api call functions
+
+  login = (email, password) => {
+    const options = config.getOptions('post')
+    options.body = {
+      email: email,
+      password: password
+    }
+    fetch(`${config.API}/api/login`, options).then(res => res.json()).then(res => {
+      window.sessionStorage.setItem('jwt', res.authToken)
+      let payload = res.authToken.split('.')[1];
+			payload = Buffer.from(payload, 'base64').toString('ascii');
+			payload = JSON.parse(payload)
+      this.setState({loggedIn: true, userId: payload.userID, companyId: payload.companyId })
+    })
+  }
+
+  signUp = () => {
+
+  }
+
+  getCompanyInfo = () => {
+    const options = config.getOptions('get')
+    if(this.state.loggedIn){
+      Promise.all([
+        fetch(`${config.API}/projects?company_id=${this.state.companyId}`, options),
+        fetch(`${config.API}/tasks?company_id=${this.state.companyId}`, options)
+      ])
+      .then(res => Promise.all([res[0].json(), res[1].json()]))
+      .then(res => this.setState({projects: [...res[0]], tasks: [...res[1]]}))
+      .catch(err => console.error(err))
+    }
+  }
+
+  //Get state functions
+
+  getTasks = () => {
+    return this.state.tasks
+  }
+
+  getProjects = () => {
+    return this.state.projects
+  }
+
+  getUserIsAdmin = () => {
+    return this.state.userIsAdmin
+  }
+
+  //Lifecycle functions
+
+  componentDidMount = () => {
+    this.getCompanyInfo()
   }
 
   renderHome = () => {
@@ -30,7 +106,12 @@ class App extends Component {
 
   render(){
     const value = {
-
+      login: this.login,
+      getTasks: this.getTasks,
+      getUserIsAdmin: this.getUserIsAdmin,
+      getProjects: this.getProjects,
+      getCompanyInfo: this.getCompanyInfo,
+      signUp: this.signUp
     }
     return (
       <ApiContext.Provider value={value}>
---
+++ src/config.js
@@ -0,0 +1,18 @@
+export default {
+    API: process.env.REACT_APP_API_ENDPOINT,
+    getOptions: method => {
+        return JSON.parse(JSON.stringify({
+			method: method.toUpperCase(),
+			headers: {
+			  'Content-Type': 'application/json',
+			  'Authorization': `bearer ${window.sessionStorage.jwt}`
+			}
+		}))
+	},
+	
+	checkForAuth: history => {
+		if(!window.sessionStorage.jwt){
+			history.push('/Login')
+		}
+	}
+}
