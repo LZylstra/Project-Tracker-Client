5922a0ef142a27dddec68a61c3b3727044f63b64
joshua hahn <tailrin.jh@gmail.com>
2020-04-03 11:00:35 -0600

    created landing page

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -3,9 +3,24 @@ import './LandingPage.css';
 
 class LandingPage extends Component {
 
+	onLogin = () => {
+		this.props.history.push('/Login')
+	}
+
+	onRegister = () => {
+		this.props.history('/SignUp')
+	}
+
+
 	render(){
 		return (
-			<div></div>
+			<div className="landing-page">
+				<p className="greeting">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<div className="button-wrapper">
+					<button onClick={this.onLogin}>Login</button>
+					<button onClick={this.onRegister}>Register</button>
+				</div>
+			</div>
 		)
 	}
 }
