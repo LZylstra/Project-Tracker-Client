bf825fb743076a4f07a1cb7bd122c8e35651866d
Arit Oyekan <aaoyekanmi4@gmail.com>
2020-04-06 01:25:58 -0500

    made project list sidebar with dummy data

--- src/ProjectList/ProjectList.js
+++ src/ProjectList/ProjectList.js
@@ -1,13 +1,54 @@
-import React, { Component } from 'react';
-import './ProjectList.css';
+import React, { Component } from "react";
+
+import "./ProjectList.css";
 
 class ProjectList extends Component {
+  dummyProjects = [
+    {
+      id: 1,
+      project_name: "test project #1",
+      priority: "High",
+    },
+    {
+      id: 2,
+      project_name: "test project #2",
+      priority: "Low",
+    },
+    {
+      id: 3,
+      project_name: "test project #3",
+      priority: "Medium",
+    },
+  ];
 
-	render(){
-		return (
-			<div></div>
-		)
-	}
+  renderProjects = (projects) => {
+    return projects.map((project) => (
+      <li key={project.id} className="project-list-item">
+        <span className="project-item-title">{project.project_name}</span>
+        <span className="project-priority">{project.priority}</span>
+      </li>
+    ));
+  };
+
+  render() {
+    return (
+      <div id="project-list">
+        <button id="add-project-button">
+          <h1>
+            Add Project<i class="add-project-plus fas fa-plus"></i>
+          </h1>
+        </button>
+        <h2 id="project-list-title">Your Projects</h2>
+        <ul>{this.renderProjects(this.dummyProjects)}</ul>
+        <div className="button-container">
+          <button>Completed</button>
+          <button>Edit</button>
+        </div>
+        <button>Delete Selected</button>
+      </div>
+    );
+  }
 }
 
 export default ProjectList;
+
