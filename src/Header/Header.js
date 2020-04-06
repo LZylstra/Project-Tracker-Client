44bb15e46183eb668955913e072b7336596597d5
joshua hahn <tailrin.jh@gmail.com>
2020-04-06 16:29:32 -0600

    added header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import './Header.css';
 
 class Header extends Component {
 
+	handleLogout = () => {
+		window.sessionStorage.removeItem("jwt")
+		window.sessionStorage.removeItem("state")
+		window.location.reload()
+	}
+
 	render(){
 		return (
-			<div></div>
+			<header className="header" id="header">
+				<span className="logo">PROJECT TRACKER</span>
+				{
+					!!window.sessionStorage.jwt ?
+						<Link to='/' onClick={this.handleLogout} id="logout">Logout</Link>
+						: <Link to='/' onClick={this.handleLogout} id="logout" disable="true"/>
+				}
+			</header>
 		)
 	}
 }
