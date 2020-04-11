d54d6bb882a8c51bf6cbd761a1aa7cc77dada4d6
LZylstra <lindseyzylstra@gmail.com>
2020-04-11 11:37:13 -0700

    fixes menu

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,39 +1,52 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
-import './Header.css';
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 class Header extends Component {
+  handleLogout = () => {
+    window.sessionStorage.removeItem("jwt");
+    window.sessionStorage.removeItem("state");
+    window.location.reload();
+  };
 
-	handleLogout = () => {
-		window.sessionStorage.removeItem("jwt")
-		window.sessionStorage.removeItem("state")
-		window.location.reload()
-	}
+  renderMenu = () => {
+    if (!!window.sessionStorage.jwt) {
+      return (
+        <>
+          <Link to="/" onClick={this.handleLogout} id="logout">
+            Logout
+          </Link>
+          <Link to="/" onClick={this.handleLogout} id="home-nav">
+            Home
+          </Link>
+          <Link to="/" onClick={this.handleLogout} id="completed-nav">
+            Completed
+          </Link>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Link
+            to="/"
+            onClick={this.handleLogout}
+            className="hidden-logout"
+            id="logout"
+            disable="true"
+          />
+        </>
+      );
+    }
+  };
+  render() {
+    return (
+      <header className="header" id="header">
+        <span className="logo">PROJECT TRACKER</span>
 
-	render(){
-		return (
-			<header className="header" id="header">
-				<span className="logo">PROJECT TRACKER</span>
-			
-				<div className= 'nav'>
-				<Link to='/' onClick={this.handleLogout} id="home-nav">Home</Link>
-				<Link to='/' onClick={this.handleLogout} id="completed-nav">Completed</Link>
-				{
-					!!window.sessionStorage.jwt ?
-					
-					
-						<Link to='/' onClick={this.handleLogout} id="logout">Logout</Link>
-						: <Link to='/' onClick={this.handleLogout} className="hidden-logout" id="logout" disable="true"/>
-						
-				}
-		
-				</div>
-
-						 
-			</header>
-		)
-	}
+        <div className="nav">{this.renderMenu()}</div>
+      </header>
+    );
+  }
 }
 
 export default Header;
--- src/config.js
+++ src/config.js
@@ -1,19 +1,20 @@
 export default {
-    API: process.env.REACT_APP_API_ENDPOINT,
-    getOptions: method => {
-        return JSON.parse(JSON.stringify({
-			method: method.toUpperCase(),
-			headers: {
-			  'Content-Type': 'application/json',
-			  'Authorization': `bearer ${window.sessionStorage.jwt}`
-			}
-		}))
-	},
-	
-	checkForAuth: history => {
-		if(!window.sessionStorage.jwt){
-			history.push('/Login')
-		}
-	}
-}
+  API: "http://localhost:8000",
+  getOptions: (method) => {
+    return JSON.parse(
+      JSON.stringify({
+        method: method.toUpperCase(),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `bearer ${window.sessionStorage.jwt}`,
+        },
+      })
+    );
+  },
 
+  checkForAuth: (history) => {
+    if (!window.sessionStorage.jwt) {
+      history.push("/Login");
+    }
+  },
+};
