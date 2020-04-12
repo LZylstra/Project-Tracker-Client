241e2d262d06a34f5404c9604529283ea4ba3761
Arit Oyekan <aaoyekanmi4@gmail.com>
2020-04-11 22:23:05 -0500

    passing all smoke tests

--- src/TaskList/TaskList.test.js
+++ src/TaskList/TaskList.test.js
@@ -1,15 +1,26 @@
 import TaskList from "./TaskList";
 import React from "react";
+import ApiContext from "../ApiContext";
+import App from "../App/App";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom";
 
+global.MutationObserver = class {
+  constructor(callback) {}
+  disconnect() {}
+  observe(element, initObject) {}
+};
 it("renders without crashing", () => {
-  // const div = document.createElement('div')
-  // ReactDOM.render(
-  // 	<BrowserRouter>
-  // 		<TaskList/>
-  // 	</BrowserRouter>,
-  // 	div
-  // )
-  // ReactDOM.unmountComponentAtNode(div)
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <BrowserRouter>
+      <ApiContext.Provider>
+        <App>
+          <TaskList />
+        </App>
+      </ApiContext.Provider>
+    </BrowserRouter>,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
 });
